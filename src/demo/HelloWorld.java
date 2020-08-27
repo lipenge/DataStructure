7d89e907ef6309e27f6ff478a545022acb85dd60
eclipse_user <eclipse_user@163.com>
2020-08-27 22:41:32 +0800

    my first commit helloworld

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+package demo;
+/**
+ * @author 李鹏
+ * @date 2020年8月27日
+ * @version 1.0
+ * 类说明 
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		char x = 'a'; 
+		char y = 97; 
+		int z = y + 1; 
+		System.out.println(x); 
+		System.out.println(y); 
+		System.out.println(z);
+
+	}
+}
